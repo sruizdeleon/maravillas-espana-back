ea5e6ea7e5d83bc86c93de620bdb40d7826f97e6
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-04-20 16:44:29 +0200

    cambiando filtro de búsqueda

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -1,93 +1,77 @@
 const Actividad = require("../models/activity.model");
 
-async function buscarActividadesPorTipoProvinciaOComunidad(tipo,proId, comunidad) {
-  if(proId){
-    console.log(proId)
-    const actividadesPorProvincia = await Actividad.find({provincia: proId})
-    .populate("provincia", "nombre");
+async function buscarActividadesPorTipoProvinciaOComunidad(tipo, proId, comunidad) {
+	const filtro = {};
 
-    return actividadesPorProvincia
-  } else if (comunidad) {
-    const actividadesPorComunidad = await Actividad.find({comunidad: comunidad})
-    .populate("provincia", "nombre");
-    return actividadesPorComunidad
-  } else if (tipo) {
-    const actividadesPorTipo = await Actividad.find({tipo: tipo})
-    .populate("provincia", "nombre");
-    return actividadesPorTipo
-  } else {
-    const todasLasActividades = await Actividad.find()
-    .populate("provincia", "nombre");;
-    return todasLasActividades
-  }
+	tipo && tipo !== "" ? filtro.tipo = tipo : false;
+	proId && proId !== "" ? filtro.provincia = proId : false;
+	comunidad && comunidad !== "" ? filtro.comunidad = comunidad : false;
+
+	const todasLasActividades = await Actividad.find(filtro).populate("provincia", "nombre imagenBandera");
+	return todasLasActividades;
 }
 
 /**
- * 
- * @param {*} id 
+ *
+ * @param {*} id
  * @returns devuelve la actividad que corresponde a dicho id
  */
 async function buscarActividadPorId(id) {
-    const actividad = await Actividad.findById(id)
-    .populate("provincia", "nombre");;
-    return actividad;
+	const actividad = await Actividad.findById(id).populate("provincia", "nombre");
+	return actividad;
 }
 
 /**
- * 
- * @param {*} actividad 
+ *
+ * @param {*} actividad
  * @returns devuelve la nueva Actividad
  */
 async function crearActividad(actividad) {
-  const nuevaActividad = new Actividad({
-    nombre: actividad.nombre,
-    img: actividad.img,
-    descripcion: actividad.descripcion,
-    provincia: actividad.provincia,
-    comunidad: actividad.comunidad,
-    tipo: actividad.tipo
-  })  
-  await nuevaActividad.save();
-  return nuevaActividad;
+	const nuevaActividad = new Actividad({
+		nombre: actividad.nombre,
+		img: actividad.img,
+		descripcion: actividad.descripcion,
+		provincia: actividad.provincia,
+		comunidad: actividad.comunidad,
+		tipo: actividad.tipo,
+	});
+	await nuevaActividad.save();
+	return nuevaActividad;
 }
 
 /**
- * 
- * @param {*} id 
+ *
+ * @param {*} id
  * @returns deveulve la actividad borrada que tenía ese id
  */
 async function borrarActividadPorId(id) {
-  const actividadBorrada = await Actividad.findByIdAndDelete(id);
-  return actividadBorrada;
+	const actividadBorrada = await Actividad.findByIdAndDelete(id);
+	return actividadBorrada;
 }
 
 /**
- * 
- * @param {*} id 
- * @param {*} actividad 
+ *
+ * @param {*} id
+ * @param {*} actividad
  * @returns devuelve la actividad de dicho id con las modificaciones
  */
 async function cambiarActividad(id, actividad) {
-    const modificacionActividad = {
-      nombre: actividad.nombre,
-      img: actividad.img,
-      descripcion: actividad.descripcion,
-      provincia: actividad.provincia,
-      comunidad: actividad.comunidad,
-      tipo: actividad.tipo,
-    };
-    const actividadModificada = await Actividad.findByIdAndUpdate(
-      id,
-      modificacionActividad
-    );
-    return actividadModificada;
-
+	const modificacionActividad = {
+		nombre: actividad.nombre,
+		img: actividad.img,
+		descripcion: actividad.descripcion,
+		provincia: actividad.provincia,
+		comunidad: actividad.comunidad,
+		tipo: actividad.tipo,
+	};
+	const actividadModificada = await Actividad.findByIdAndUpdate(id, modificacionActividad);
+	return actividadModificada;
 }
 
 module.exports = {
-    buscarActividadesPorTipoProvinciaOComunidad,
-    buscarActividadPorId,
-    crearActividad,
-    borrarActividadPorId,
-    cambiarActividad
-}
+	buscarActividadesPorTipoProvinciaOComunidad,
+	buscarActividadPorId,
+	crearActividad,
+	borrarActividadPorId,
+	cambiarActividad,
+};
